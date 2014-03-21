b44394d297ad0d6049b4a074d67e25c08e5ccea0
Leonardo Henrique Tsuda <leonardo.tsuda@gmail.com>
2014-03-21 15:26:22 -0300

    create netbeans project with basics classes

---
+++ chess_java/xadrez_VoIP/src/voIP/Peca.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package voIP;
+
+/**
+ *
+ * @author leonardohenriquetsuda
+ */
+public class Peca {
+	private int coord_x;
+	private int coord_y;
+	private char simbolo;
+
+	/**
+	 * @return the coord_x
+	 */
+	public int getCoord_x() {
+		return coord_x;
+	}
+
+	/**
+	 * @param coord_x the coord_x to set
+	 */
+	public void setCoord_x(int coord_x) {
+		this.coord_x = coord_x;
+	}
+
+	/**
+	 * @return the coord_y
+	 */
+	public int getCoord_y() {
+		return coord_y;
+	}
+
+	/**
+	 * @param coord_y the coord_y to set
+	 */
+	public void setCoord_y(int coord_y) {
+		this.coord_y = coord_y;
+	}
+
+	/**
+	 * @return the simbolo
+	 */
+	public char getSimbolo() {
+		return simbolo;
+	}
+}
+
