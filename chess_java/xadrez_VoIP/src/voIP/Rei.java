1841669d916925f22f4c5db59aca7ce73472fcc0
Christian Sampaio <christian.fsampaio@gmail.com>
2014-04-10 01:46:03 -0300

    validando movimento antes de executar

--- chess_java/xadrez_VoIP/src/voIP/Rei.java
+++ chess_java/xadrez_VoIP/src/voIP/Rei.java
@@ -30,20 +30,12 @@ public class Rei extends Peca{
         if (posicao.x > 7 || posicao.y > 7 || posicao.x < 0 || posicao.y < 0) {
         return false;
         }
-        
-        switch (simbolo){
-            case 'k':
-            case 'K':
 
-                if (Math.abs(posicao.y - this.posicao.y) <= 1 && Math.abs(posicao.x - this.posicao.x) <= 1)
-                {
-                    return true;
-                }
-                break;
-            default:
-            break;
+        if (Math.abs(posicao.y - this.posicao.y) <= 1 && Math.abs(posicao.x - this.posicao.x) <= 1)
+        {
+            return true;
         }
-    return false;
+        return false;
     }
 
     @Override
